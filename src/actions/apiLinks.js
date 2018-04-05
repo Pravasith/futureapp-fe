02881e6e2ded205bee311116910be58d3cd9b678
Pravasith <pravasith@gmail.com>
2018-04-05 16:29:25 +0530

    testingsss

--- src/actions/apiLinks.js
+++ src/actions/apiLinks.js
@@ -1,6 +1,6 @@
 
-// const API_BASE_URL = 'http://159.89.167.240:8000/'
-const API_BASE_URL = 'http://localhost:8000/'
+const API_BASE_URL = 'http://159.89.167.240:8000/'
+// const API_BASE_URL = 'http://localhost:8000/'
 const API_PATH = 'api/'
 const API_URL = API_BASE_URL + API_PATH
 
